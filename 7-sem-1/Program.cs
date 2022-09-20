9b834928f5cec7cb646b414c47dfecfb6793bae2
Alex Karnakov <ak2005@yandex.ru>
2022-09-20 22:08:34 +0300

    Init commit

---
+++ 7-sem-1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 46: 
+// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4. 
+// 1 4 8 19
+// 5 -2 33 -2 
+// 77 3 8 1
+
+int[,] CreateArrayRandomNumbers(int row, int column)
+{
+    var random = new Random();
+    int[,] resultArray = new int[row, column];
+
+    for ( int i = 0; i < row; i++)
+    {
+        for ( int j = 0; j < column; j++)
+        {
+            resultArray[i,j] = random.Next(-100, 101);
+        }
+    } 
+    return resultArray;
+}
+
+// Print array to Console
+void PrintArrayToConsole(int[,] array)
+{
+    // Console.Write($"[ ");
+    for ( int i = 0; i < array.GetLength(0); i++)
+    {
+        for ( int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine();
+
+Console.Write("Input number of columns: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = CreateArrayRandomNumbers(row, column);
+
+PrintArrayToConsole(newArray);
+
+
+// int row = 0;
+// int column = 0;
+// Console.Write("Input number of rows: ");
+// bool parseIsOk = int.TryParse(Console.ReadLine(), out int number));
+// if ( !parseIsOk )
+// {
+//     Console.WriteLine($"{array[i]} ]");
+//     return
+// }
+// else result = number
---
+++ 7-sem-2/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 48: 
+// Задайте двумерный массив размера m на n, каждый элемент в массиве 
+// находится по формуле: Aₙₙ = m+n. Выведите полученный массив на экран.
+//  m = 3, n = 4. 
+// 0 1 23
+// 1 2 34
+// 2 3 45
+
+
+// int[,] CreateArrayRandomNumbers(int row, int column)
+// {
+//     var random = new Random();
+//     int[,] resultArray = new int[row, column];
+
+//     for ( int i = 0; i < row; i++)
+//     {
+//         for ( int j = 0; j < column; j++)
+//         {
+//             if ( i % 2 == 0 && j % 2 == 0)
+//             {
+//                 resultArray[i,j] = (int)(Math.Pow(random.Next(-100, 101), 2));
+//             }
+//             else
+//             {
+//                 resultArray[i,j] = random.Next(-100, 101);
+//             }
+            
+//         }
+//     } 
+//     return resultArray;
+// }
+
+int[,] CreateArrayRandomNumbers(int row, int column)
+{
+    var random = new Random();
+    int[,] resultArray = new int[row, column];
+
+    for ( int i = 0; i < row; i++)
+    {
+        for ( int j = 0; j < column; j++)
+        {
+            resultArray[i,j] = random.Next(0, 50);
+        }
+    } 
+    return resultArray;
+}
+
+int SumElementDiagonal(int[,] array)
+{
+    int sum = 0;
+    int min = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    if ( col < min ) min = col;
+
+    for ( int i = 0; i < min; i++)
+    {
+        sum = sum + array[i, i];
+    } 
+    return sum;
+}
+
+// Print array to Console
+void PrintArrayToConsole(int[,] array)
+{
+    // Console.Write($"[ ");
+    for ( int i = 0; i < array.GetLength(0); i++)
+    {
+        for ( int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine();
+
+Console.Write("Input number of columns: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = CreateArrayRandomNumbers(row, column);
+int sumArray = SumElementDiagonal(newArray);
+
+PrintArrayToConsole(newArray);
+Console.WriteLine($"{sumArray}");
+
+
