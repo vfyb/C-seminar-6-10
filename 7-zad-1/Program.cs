23a76f70578800ae0fae4e09fd26d646abb0a391
Alex Karnakov <ak2005@yandex.ru>
2022-09-23 17:12:57 +0300

    Create array with random double elements

--- 7-zad-1/Program.cs
+++ 7-zad-1/Program.cs
@@ -46,7 +46,6 @@ double[,] CreateArrayRandomNumbers(int row, int column)
 // Print array to Console
 void PrintArrayToConsole(double[,] array)
 {
-    // Console.Write($"[ ");
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
