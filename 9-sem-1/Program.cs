6f6487fa17c6039ef1b14c7379bab4f4475a09c1
Alex Karnakov <ak2005@yandex.ru>
2022-09-29 14:42:41 +0300

    Bubble sort

---
+++ 9-sem-1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Bubble sort
+
+bool SearchElem(int[] array, int value)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == value)
+        {
+            return true;
+        }
+    }
+}
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    bool flag = false;
+    while (flag != true)
+    {
+        flag = false;
+
+        int temp = new Random().Next(1, 100);
+        if (SearchElem(array, temp))
+        {
+            continue;
+        }
+        else
+        {
+            array[i] = temp;
+            flag = true;
+        }
+    }
+}
