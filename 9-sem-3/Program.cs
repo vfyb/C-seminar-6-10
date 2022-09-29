018d43da117ded6f482f0eb1241613ec3612000e
Alex Karnakov <ak2005@yandex.ru>
2022-09-29 14:41:20 +0300

    Sum of digits in number

---
+++ 9-sem-3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: 
+// Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+// 453 -> 12 45 -> 9
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    if (num > 0)
+    {
+        sum = SumDigit(num / 10) + (num % 10);
+    }
+    return sum;
+}
+
+Console.WriteLine("Input number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine(sumDigit);
