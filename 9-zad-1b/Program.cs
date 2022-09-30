3c422838b67dc11a237c3fc2f511c8ca1e6e9dd1
Alex Karnakov <ak2005@yandex.ru>
2022-09-30 15:29:54 +0300

    Print natural numbers from N to 1

---
+++ 9-zad-1b/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Get int number from user
+int GetAnyIntNumber(string text)
+{
+    Console.WriteLine($"Input {text}: ");
+
+    while (true)
+    {
+        try
+        {
+            int result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        }
+        catch
+        {
+            Console.WriteLine("No number was entered. Try again:");
+        }
+    }
+}
+
+// Get string with natual numbers
+string NaturalNumbers(int num)
+{
+    string result = string.Empty;
+
+    if (num == 1)
+    {
+        result = "1";
+    }
+    else
+    {
+        result =$"{num}" + ", " + NaturalNumbers(num - 1);
+    }
+    return result;
+}
+
+int number = GetAnyIntNumber("number");
+string natNum = string.Empty;
+
+natNum = NaturalNumbers(number);
+Console.WriteLine(natNum);
